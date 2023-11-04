b35d401f6d779f4ff753c884da4bf831ba6cc098
Choi Tae gyu <suker800@gmail.com>
2023-11-04 17:24:56 +0900

    first commit

---
+++ src/main/java/com/example/skptemp/SkpTempApplication.java
@@ -0,0 +1,13 @@
+package com.example.skptemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkpTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkpTempApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skptemp/SkpTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skptemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkpTempApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
