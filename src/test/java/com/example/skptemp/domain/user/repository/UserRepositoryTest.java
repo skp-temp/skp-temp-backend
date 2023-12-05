e78aa790095365dfb37d8e711d89a6a1ac3dd9ea
dongHun <actgong@gmail.com>
2023-12-06 01:14:30 +0900

    feature - 코드 발급 테스트 코그 작성

---
+++ src/test/java/com/example/skptemp/domain/user/repository/UserRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.example.skptemp.domain.user.repository;
+
+import com.example.skptemp.domain.user.entity.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String TEST_FIRST_NAME = "강";
+    private static final String TEST_LAST_NAME = "동훈";
+    private static final Long TEST_KAKAO_ID = 12341234L;
+
+    @Test
+    void 코드_생성_성공(){
+        //given
+        //when
+        User testUser = User.createUser(TEST_FIRST_NAME, TEST_LAST_NAME, TEST_KAKAO_ID);
+        userRepository.save(testUser);
+        User findUser = userRepository.findById(testUser.getId()).get();
+
+        //then
+        Assertions.assertThat(testUser.getCode()).isNotEmpty();
+        Assertions.assertThat(testUser.getAuthority()).isNotEmpty();
+        Assertions.assertThat(testUser.getCode()).isEqualTo(findUser.getCode());
+        Assertions.assertThat(testUser.getAuthority()).isEqualTo(findUser.getAuthority());
+    }
+}
